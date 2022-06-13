d025d8d5c19ebbb5819d1d669bfd1d83f7525b03
Валерия <101348978+valesa-st@users.noreply.github.com>
2022-06-13 12:39:15 +0300

    Create calculator.py
    Код калькулятора.

---
+++ app/calculator.py
@@ -0,0 +1,13 @@
+class Calculator:
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divizion(self, x, y):
+        return x / y
+
+    def substraction(self, x, y):
+        return x - y
+
+    def adding(self, x, y):
+        return x + y
